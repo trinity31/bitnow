4e90dddaf0d9d8a2afdb63f0d74309190d1d32fa
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2024-12-27 17:28:55 +0900

    App theme

---
+++ lib/core/theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static const primaryColor = Color.fromARGB(255, 236, 239, 6); // 네온 핑크
+  static const backgroundColor = Color(0xFF0A0E17); // 다크 네이비
+  static const cardColor = Color(0xFF1A1F2C); // 약간 밝은 다크 네이비
+  static const textColor = Colors.white;
+  static const secondaryTextColor = Color(0xFF8F9BB3); // 연한 회색
+  static const accentColor = Color(0xFF00D1FF); // 네온 블루
+
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: backgroundColor,
+    brightness: Brightness.dark,
+    primaryColor: const Color.fromARGB(255, 236, 239, 6),
+    colorScheme: const ColorScheme.dark(
+      primary: Color.fromARGB(255, 236, 239, 6),
+      secondary: accentColor,
+      surface: backgroundColor,
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: backgroundColor,
+      elevation: 0,
+    ),
+    textTheme: GoogleFonts.interTextTheme(
+      ThemeData.dark().textTheme,
+    ),
+    cardTheme: const CardTheme(
+      color: cardColor,
+      elevation: 0,
+    ),
+  );
+}
