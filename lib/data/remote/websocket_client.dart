664880ef0547d7e06a2c74792dbaddbf24aca943
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2025-01-08 12:11:57 +0900

    Reconnect web socket on error

--- lib/data/remote/websocket_client.dart
+++ lib/data/remote/websocket_client.dart
@@ -2,10 +2,13 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:btc_price_app/domain/model/websocket_response.dart';
 import 'dart:convert';
 import '../../utils/print.dart';
+import 'dart:async';
 
 class WebSocketClient {
   WebSocketChannel? _channel;
   final String baseUrl;
+
+  final _controller = StreamController<WebSocketResponse>.broadcast();
 
   WebSocketClient({required this.baseUrl});
 
@@ -15,21 +18,39 @@ class WebSocketClient {
         Uri.parse('$baseUrl/ws/price'),
       );
       safePrint('WebSocket 연결 성공');
+
+      _channel!.stream.listen(
+        (data) {
+          final jsonMap = jsonDecode(data);
+          _controller.add(WebSocketResponse.fromJson(jsonMap));
+        },
+        onError: (error) {
+          safePrint('WebSocket 에러 발생: $error');
+          reconnect();
+        },
+        onDone: () {
+          safePrint('WebSocket 연결이 종료되었습니다.');
+          reconnect();
+        },
+      );
     } catch (e) {
       safePrint('WebSocket 연결 실패: $e');
       rethrow;
     }
   }
 
-  Stream<WebSocketResponse> get priceStream => _channel!.stream.map((data) {
-        final json = jsonDecode(data);
-        return WebSocketResponse.fromJson(json);
-      });
+  Stream<WebSocketResponse> get priceStream => _controller.stream;
+
+  bool get isConnected => _channel != null;
+
+  void reconnect() {
+    safePrint('웹소켓 재연결 시도');
+    disconnect();
+    connect();
+  }
 
   void disconnect() {
     _channel?.sink.close();
     _channel = null;
   }
-
-  bool get isConnected => _channel != null;
 }
